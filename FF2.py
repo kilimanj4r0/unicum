0a9946356aa369c00fa60701289aa6301d21d67f
kilimanj4r0 <43704968+kilimanj4r0@users.noreply.github.com>
2020-01-19 19:57:37 +0700

    Add files via upload

--- FF2.py
+++ FF2.py
@@ -1,37 +1,62 @@
-#https://github.com/aristofun/py-agender
-import cv2
+import os
 import time
+
+import cv2
 from pyagender import PyAgender
-cap = cv2.VideoCapture(0)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # параметры могут не применяться к большинству камер
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 #cap.set(3, 480)  # set width of the frame
 #cap.set(4, 640)  # set height of the frame
-while True:
-    start = time.time()
-    ret, image = cap.read()
-    faces = PyAgender().detect_genders_ages(image)
-    #gender > 0.5 == female
-    print(faces)
-    for data in faces:
-        for key in data:
-            if(key=='gender' or key == 'age'):
-                if key=='gender':
-                    if data[key] <0.5:
-                        #print(key)
-                        print('Male')
-                    else:
-                        #print(key)
-                        print('Female')
-                if key=='age':
-                      #print(key)
-                      print(data[key])
-    cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('frame', image)
-    print("--- %s seconds ---" % (time.time() - start))
-    if cv2.waitKey(1) & 0xFF == ord('q'): # чтобы завершить программу нажать q на ENG раскладке
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+def detect(image):
+    agenda = PyAgender()
+    ans = agenda.detect_genders_ages(image)
+    return ans
+
+def memory():
+    import os
+    import psutil
+    pid = os.getpid()
+    py = psutil.Process(pid)
+    memoryUse = py.memory_info()[0]/2.**30  # memory use in GB...I think
+    print('memory use:', memoryUse)
+
+def mainf():
+    while True:
+        start = time.time()
+        ret, image = cap.read()
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        ans = detect(image)
+        #agenda = PyAgender()
+        #ans = agenda.detect_genders_ages(image)
+        #gender > 0.5 == female
+        print(ans)
+        for data in ans:
+            for key in data:
+                if(key=='gender' or key == 'age'):
+                    if key=='gender':
+                        if data[key] <0.5:
+                            #print(key)
+                            print('Male')
+                        else:
+                            #print(key)
+                            print('Female')
+                    if key=='age':
+                          #print(key)
+                          print(data[key])
+        print("--- %s seconds ---" % (time.time() - start))
+        cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('frame', image)
+        memory()
+        #tensorflow.contrib.keras.backend.clear_session()
+        if cv2.waitKey(1) & 0xFF == ord('q'): # чтобы завершить программу нажать q на ENG раскладке
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+if __name__ == "__main__":
+    mainf()
 # OUTPUT EXAMPLE:
 #[{'left': 65, 'top': 57, 'right': 290, 'bottom': 282, 'width': 225, 'height': 225, 'gender': 0.0150823705, 'age': 19.3277230063934}]
 #Male
